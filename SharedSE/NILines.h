a8a96e368af2ba5e4545e203a9aa7233b4291db3
NullCascade <nullcascade@gmail.com>
2023-01-25 08:02:13 -0800

    Add NiLines::getModelData() convenience function.

--- SharedSE/NILines.h
+++ SharedSE/NILines.h
@@ -2,10 +2,14 @@
 
 #include "NIGeometry.h"
 
+#include "NILinesData.h"
+
 namespace NI {
 	struct Lines : Geometry {
 		Lines(unsigned short vertexCount, Vector3* vertices, PackedColor* colors, Vector2* textureCoords, bool* lineSegmentFlags);
 		~Lines();
+
+		LinesData* getModelData() { return static_cast<LinesData*>(modelData.get()); }
 
 		static Pointer<Lines> create(unsigned short vertexCount, bool useColors = false, bool useTextureCoords = false);
 		static Pointer<Lines> create(unsigned short vertexCount, Vector3* vertices, PackedColor* colors, Vector2* textureCoords, bool* lineSegmentFlags);

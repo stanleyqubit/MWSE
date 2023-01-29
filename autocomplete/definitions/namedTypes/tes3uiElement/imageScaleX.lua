fe39f7d34b1f04ffec3f6ae8f4f2eac3fb9f5c48
Hrnchamd <iamalannoble@hotmail.com>
2023-01-29 22:55:57 +0000

    Support image mirroring on `tes3uiElement`s by using negative imageScaleX/Y.

--- autocomplete/definitions/namedTypes/tes3uiElement/imageScaleX.lua
+++ autocomplete/definitions/namedTypes/tes3uiElement/imageScaleX.lua
@@ -1,5 +1,5 @@
 return {
 	type = "value",
-	description = [[Image scaling multipliers. Only applies to image elements.]],
+	description = [[Image scaling multipliers. Only applies to image elements. `0` disables scaling and displays the original image. Negative numbers will mirror and scale along this axis.]],
 	valuetype = "number",
 }
--- autocomplete/definitions/namedTypes/tes3uiElement/scaleMode.lua
+++ autocomplete/definitions/namedTypes/tes3uiElement/scaleMode.lua
@@ -1,5 +1,5 @@
 return {
 	type = "value",
-	description = [[When set to `true` on image and NIF elements, they are scaled to fit `width` and `height`.]],
+	description = [[When set to `true` on image and NIF elements, the image or NIF is scaled to fit the element `width` and `height`.]],
 	valuetype = "boolean",
 }

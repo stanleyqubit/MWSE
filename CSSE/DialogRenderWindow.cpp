d64324a5322ddfdf81eaa315d1d099b284202b10
Greatness7 <Greatness7@gmail.com>
2023-01-31 16:30:50 -0500

    CSSE: Various fixes and improvements to the group scaling feature.
    Move group scaling to a settings toggle (rather than holding ALT).
    Make scaling relative to the last target, like movement/rotation.
    Implement scale clamping to better preserve relative positions.
    Fix reference lighting not being updated when group scaling.

--- CSSE/DialogRenderWindow.cpp
+++ CSSE/DialogRenderWindow.cpp
@@ -300,35 +300,40 @@ namespace se::cs::dialog::render_window {
 		using windows::isKeyDown;
 
 		auto selectionData = SelectionData::get();
-		if (!isKeyDown(VK_MENU) || selectionData->numberOfTargets == 1) {
+		bool useGroupScaling = settings.render_window.use_group_scaling;
+		if (!useGroupScaling || selectionData->numberOfTargets == 1) {
 			const auto VanillaScalingHandler = reinterpret_cast<void(__cdecl*)(RenderController*, SelectionData::Target*, int)>(0x404949);
 			VanillaScalingHandler(renderController, firstTarget, scaler);
 			return;
 		}
 
-		const auto scaleDelta = scaler * 0.01f;
+		// Clamp such that no reference will be scaled beyond supported bounds. [0.5, 2.0]
+		auto scaleDelta = scaler * 0.01f;
+		for (auto target = firstTarget; target; target = target->next) {
+			const auto scale = target->reference->getScale();
+			scaleDelta = std::clamp(scaleDelta, 0.5f - scale, 2.0f - scale);
+		}
 
-		const auto& center = selectionData->bound.center;
+		// Scale all selected references and their positions relative to the last target.
+		const auto reference = selectionData->getLastTarget()->reference;
+		const auto distance = 1.0f + scaleDelta / reference->getScale();
+		const auto origin = reference->position;
 
 		for (auto target = firstTarget; target; target = target->next) {
 			auto reference = target->reference;
+			reference->setScale(reference->getScale() + scaleDelta);
 
-			const auto oldScale = reference->getScale();
-			reference->setScale(oldScale + scaleDelta);
+			const auto offset = reference->position - origin;
+			reference->position = origin + (offset * distance);
+			reference->unknown_0x10 = reference->position;
+			reference->sceneNode->localTranslate = reference->position;
+			reference->sceneNode->update(0.0f, true, true);
 
-			const auto newScale = reference->getScale();
-			if (newScale != oldScale) {
-				const auto offset = reference->position - center;
-				const auto multiplier = newScale / oldScale;
+			DataHandler::get()->updateLightingForReference(reference);
 
-				reference->position = center + offset * multiplier;
-				reference->unknown_0x10 = reference->position;
-				reference->sceneNode->localTranslate = reference->position;
-				reference->sceneNode->update(0.0f, true, true);
-
-				reference->setAsEdited();
-			}
+			reference->setAsEdited();
 		}
+		selectionData->recalculateBound();
 	}
 
 	//
@@ -1064,6 +1069,7 @@ namespace se::cs::dialog::render_window {
 			SET_SNAPPING_AXIS_NEGATIVE_Y,
 			SET_SNAPPING_AXIS_POSITIVE_Z,
 			SET_SNAPPING_AXIS_NEGATIVE_Z,
+			USE_GROUP_SCALING,
 			USE_LEGACY_OBJECT_MOVEMENT,
 			USE_WORLD_AXIS_ROTATION,
 			SAVE_STATE_TO_QUICKSTART,
@@ -1144,6 +1150,14 @@ namespace se::cs::dialog::render_window {
 		menuItem.hSubMenu = subMenuSnappingAxis.hSubMenu;
 		menuItem.dwTypeData = (LPSTR)"Set &Snapping Axis";
 		InsertMenuItemA(menu, index++, TRUE, &menuItem);
+
+		menuItem.wID = USE_GROUP_SCALING;
+		menuItem.fMask = MIIM_FTYPE | MIIM_CHECKMARKS | MIIM_STRING | MIIM_ID;
+		menuItem.fType = MFT_STRING;
+		menuItem.fState = (settings.render_window.use_group_scaling) ? MFS_CHECKED : MFS_UNCHECKED;
+		menuItem.dwTypeData = (LPSTR)"Use Group Scaling";
+		InsertMenuItemA(menu, index++, TRUE, &menuItem);
+		CheckMenuItem(menu, USE_GROUP_SCALING, (settings.render_window.use_group_scaling) ? MFS_CHECKED : MFS_UNCHECKED);
 
 		menuItem.wID = USE_LEGACY_OBJECT_MOVEMENT;
 		menuItem.fMask = MIIM_FTYPE | MIIM_CHECKMARKS | MIIM_STRING | MIIM_ID;
@@ -1234,6 +1248,10 @@ namespace se::cs::dialog::render_window {
 			break;
 		case SET_SNAPPING_AXIS_NEGATIVE_Z:
 			snappingAxis = SnappingAxis::NEGATIVE_Z;
+			break;
+		case USE_GROUP_SCALING:
+			settings.render_window.use_group_scaling = !settings.render_window.use_group_scaling;
+			settings.save();
 			break;
 		case USE_LEGACY_OBJECT_MOVEMENT:
 			settings.render_window.use_legacy_object_movement = !settings.render_window.use_legacy_object_movement;

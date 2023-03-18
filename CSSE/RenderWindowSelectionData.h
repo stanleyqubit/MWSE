7bd382b685a99726632e6270a87a3bb07aa0f99b
Hrnchamd <iamalannoble@hotmail.com>
2023-03-18 12:37:15 +0000

    CSSE: Add render window undo manager.

--- CSSE/RenderWindowSelectionData.h
+++ CSSE/RenderWindowSelectionData.h
@@ -3,6 +3,7 @@
 #include "CSDefines.h"
 
 #include "NIBound.h"
+#include "CSReference.h"
 
 #include "MemoryUtil.h"
 
@@ -34,4 +35,42 @@ namespace se::cs::dialog::render_window {
 
 		Target* getLastTarget() const;
 	};
+	static_assert(sizeof(SelectionData) == 0x18, "SelectionData failed size validation");
+	static_assert(sizeof(SelectionData::Target) == 0xC, "SelectionData::Target failed size validation");
+
+	struct UndoManager {
+		enum class Action : unsigned int {
+			Deleted = 1,
+			Added = 2,
+			Moved = 3,
+			Land_related_4 = 4,
+			Land_related_5 = 5,
+			Land_related_6 = 6,
+		};
+		struct Undoable {
+			int groupIndex; // 0x0
+			int groupTotal; // 0x4
+			Reference* reference; // 0x8
+			Action action; // 0xC
+			NI::Vector3 unknown_10; // 0x10
+			NI::Vector3 unknown_1C; // 0x1C
+			float scale; // 0x28
+			int unknown_2C[7]; // 0x2C
+			Undoable* nextRedo; // 0x48
+			Undoable* nextUndo; // 0x4C
+		};
+
+		Undoable* sentinel; // 0x0
+		Undoable* currentUndoPosition; // 0x4
+
+		void storeCheckpoint(Action action); // When using Action::Moved you must have a checkpoint both before and after the action or it will crash later on.
+		void undo();
+		void redo();
+
+		static inline auto get() {
+			return memory::ExternalGlobal<UndoManager*, 0x6CEA24>::get();
+		}
+	};
+	static_assert(sizeof(UndoManager) == 0x8, "UndoManager failed size validation");
+	static_assert(sizeof(UndoManager::Undoable) == 0x50, "UndoManager::Undoable failed size validation");
 }

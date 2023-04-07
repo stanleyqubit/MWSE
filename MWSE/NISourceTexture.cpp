e9d89c5e71fdca3f2663ea7f122a9a365376e1c5
Hrnchamd <iamalannoble@hotmail.com>
2023-04-07 21:54:36 +0100

    Change `niPixelData.createSourceTexture` to not preload textures, so that the isStatic flag can be set to the desired value before the renderer sees the texture.

--- MWSE/NISourceTexture.cpp
+++ MWSE/NISourceTexture.cpp
@@ -3,14 +3,23 @@
 #include "TES3DataHandler.h"
 
 namespace NI {
-	const auto NI_SourceTexture_createFromPath = reinterpret_cast<SourceTexture*(__cdecl*)(const char*, SourceTexture::FormatPrefs *)>(0x6DE7F0);
-	Pointer<SourceTexture> SourceTexture::createFromPath(const char* path, SourceTexture::FormatPrefs * formatPrefs) {
+	const auto NI_SourceTexture_bPreload = reinterpret_cast<bool*>(0x7C4C90);
+
+	const auto NI_SourceTexture_createFromPath = reinterpret_cast<SourceTexture*(__cdecl*)(const char*, SourceTexture::FormatPrefs*)>(0x6DE7F0);
+	Pointer<SourceTexture> SourceTexture::createFromPath(const char* path, SourceTexture::FormatPrefs* formatPrefs) {
 		return NI_SourceTexture_createFromPath(path, formatPrefs);
 	}
 
-	const auto NI_SourceTexture_createFromPixelData = reinterpret_cast<SourceTexture*(__cdecl*)(PixelData*, SourceTexture::FormatPrefs *)>(0x6DE940);
-	Pointer<SourceTexture> SourceTexture::createFromPixelData(PixelData* pixelData, SourceTexture::FormatPrefs * formatPrefs) {
-		return NI_SourceTexture_createFromPixelData(pixelData, formatPrefs);
+	const auto NI_SourceTexture_createFromPixelData = reinterpret_cast<SourceTexture*(__cdecl*)(PixelData*, SourceTexture::FormatPrefs*)>(0x6DE940);
+	Pointer<SourceTexture> SourceTexture::createFromPixelData(PixelData* pixelData, SourceTexture::FormatPrefs* formatPrefs) {
+		// Turn off texture preloading, to allow setting the isStatic flag before the renderer builds the texture.
+		bool preload = false;
+		std::swap(*NI_SourceTexture_bPreload, preload);
+
+		auto result = NI_SourceTexture_createFromPixelData(pixelData, formatPrefs);
+
+		std::swap(*NI_SourceTexture_bPreload, preload);
+		return result;
 	}
 
 	void SourceTexture::loadPixelDataFromFile() {

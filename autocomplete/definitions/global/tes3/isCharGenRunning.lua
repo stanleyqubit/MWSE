28852d0e72301f6a36aece0ec232ebbb19ed1a3f
Pete Goodfellow <petethegoat@gmail.com>
2023-05-28 14:36:53 +0100

    Add proper definitions for tes3.isCharGenRunning and tes3.isCharGenStarted

---
+++ autocomplete/definitions/global/tes3/isCharGenRunning.lua
@@ -0,0 +1,5 @@
+return {
+	type = "function",
+	description = [[Returns `true` if the character generation process is still in progress for the current player character.]],
+	valuetype = "boolean",
+}
---
+++ autocomplete/definitions/global/tes3/isCharGenStarted.lua
@@ -0,0 +1,5 @@
+return {
+	type = "function",
+	description = [[Returns `true` if the character generation process has been started (this is only false on the main menu).]],
+	valuetype = "boolean",
+}
